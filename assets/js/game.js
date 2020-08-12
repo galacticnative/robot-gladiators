f35d9b5cc4acb22f666ea78f1a2fa0030eb99d08
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-12 12:54:54 -0700

    added end game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,7 +75,12 @@ var fight = function(enemyName) {
     
     }
 };
-  
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
     for(var i = 0; i < enemyNames.length; i++) {
         //if player still alive, keep fighting
         if (playerHealth > 0) {
@@ -89,7 +94,7 @@ var fight = function(enemyName) {
             enemyHealth = 50;
 
             // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
+            //debugger;
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
@@ -98,5 +103,35 @@ var fight = function(enemyName) {
         //if player is not alive, stop the game
         else {
             window.alert("You have lost your robot in battle! Game Over!");
+            break;
         }
     }
+
+    //after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
+
+//function to end the entire game
+var endGame = function() {
+    //if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("The game has now ended. Let's see how you did!");
+    }
+
+    //ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+//start the game when the page loads
+startGame();
