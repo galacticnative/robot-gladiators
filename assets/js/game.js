99fe9116b1bda56b51898c420e59d83a23f482c4
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-08-14 02:10:51 -0700

    added parseInt for shop prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -131,21 +131,20 @@ var endGame = function () {
 var shop = function() {
     //ask player what they'd like to do (replaced the console.log)
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE, to make a choice."
     );
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     //use SWITCH to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": //new case
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE": //new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE": //new case
-        case "leave": 
+        case 3:
             window.alert("Leaving the store.");
 
             //do nothing, so function will end
@@ -235,7 +234,7 @@ var playerInfo = {
             this.money -= 7;
         }
         else {
-            window.alert("You don't have enought money!");
+            window.alert("You don't have enough money!");
         }
     }
 };
